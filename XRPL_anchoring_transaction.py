4cdfaff950c427099012db97d57762765627acc3
zshahzadpumacy <zshahzadpumacy@gmail.com>
2023-06-24 13:05:38 +0200

    Anchoring Transaction added

---
+++ XRPL_anchoring_transaction.py
@@ -0,0 +1,36 @@
+import xrpl
+from xrpl.clients import JsonRpcClient
+from xrpl.wallet import Wallet
+
+
+JSON_RPC_URL = "https://s.altnet.rippletest.net:51234/"
+client = JsonRpcClient(JSON_RPC_URL)
+
+# Credentials to be anchored
+verifiable_credentials = {
+    "name": "John Doe",
+    "email": "johndoe@gmail.com",
+    "age": 30
+}
+
+def Anchor():
+    #Prepare Transaction
+    transaction = xrpl.models.transactions.Payment(
+        account="raBiQyQUWvGiUEt8YW9ThW2WdnM8rajFL3",  # Replace with the sender's XRP address
+        destination="rBjFFJkrfTvTwSzJGSTw45iGKjxxtoFXGc",  # Replace with the destination XRP address
+        amount=xrpl.models.amounts.Amount("1", "XRP"),  # Replace with the desired transaction amount
+        invoice_id=xrpl.utils.crypto.hash_sha256(str(verifiable_credentials)),  # Hash the credentials as the invoice ID
+        fee=xrpl.models.amounts.Amount("12", "drops")  # Set the transaction fee
+)
+
+    # Credentials
+    test_wallet = Wallet(seed="sEd7AB2cFh5XK8Y5jAXLscPzPGym4VL", sequence=38924475)
+
+    # Signing Transaction
+    my_tx_payment_signed = xrpl.transaction.safe_sign_and_autofill_transaction(transaction, test_wallet, client)
+
+    # Submit and send the transaction
+    tx_response = xrpl.transaction.send_reliable_submission(my_tx_payment_signed, client)
+    print(tx_response)
+
+Anchor()
